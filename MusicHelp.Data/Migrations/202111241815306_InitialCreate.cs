864e328ff563a8d08f17316f61f0413ba251eaac
Joe.Micucci <micucci.joseph@gmail.com>
2021-11-29 14:40:26 -0500

    Azure Set Up, and Getting Tabs to Work for Index

---
+++ MusicHelp.Data/Migrations/202111241815306_InitialCreate.cs
@@ -0,0 +1,162 @@
+﻿namespace MusicHelp.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Instrument",
+                c => new
+                    {
+                        InstrumentID = c.Int(nullable: false, identity: true),
+                        OwnerID = c.Guid(nullable: false),
+                        InstrumentName = c.String(nullable: false),
+                        CreatedUtc = c.DateTimeOffset(nullable: false, precision: 7),
+                        ModifiedUtc = c.DateTimeOffset(precision: 7),
+                    })
+                .PrimaryKey(t => t.InstrumentID);
+            
+            CreateTable(
+                "dbo.Lesson",
+                c => new
+                    {
+                        LessonID = c.Int(nullable: false, identity: true),
+                        OwnerID = c.Guid(nullable: false),
+                        InstrumentID = c.Int(nullable: false),
+                        LessonName = c.String(nullable: false),
+                        LessonDescription = c.String(nullable: false),
+                        LessonDifficulty = c.Int(nullable: false),
+                        LessonSource = c.String(nullable: false),
+                        LessonLink = c.String(nullable: false),
+                        CreatedUtc = c.DateTimeOffset(nullable: false, precision: 7),
+                        ModifiedUtc = c.DateTimeOffset(precision: 7),
+                    })
+                .PrimaryKey(t => t.LessonID)
+                .ForeignKey("dbo.Instrument", t => t.InstrumentID)
+                .Index(t => t.InstrumentID);
+            
+            CreateTable(
+                "dbo.Tablature",
+                c => new
+                    {
+                        TabID = c.Int(nullable: false, identity: true),
+                        InstrumentID = c.Int(nullable: false),
+                        LessonID = c.Int(nullable: false),
+                        OwnerID = c.Guid(nullable: false),
+                        TabName = c.String(nullable: false),
+                        TabArtist = c.String(nullable: false),
+                        TabAlbum = c.String(),
+                        TabDifficulty = c.Int(nullable: false),
+                        TabSource = c.String(nullable: false),
+                        TabLink = c.String(nullable: false),
+                        CreatedUtc = c.DateTimeOffset(nullable: false, precision: 7),
+                        ModifiedUtc = c.DateTimeOffset(precision: 7),
+                    })
+                .PrimaryKey(t => t.TabID)
+                .ForeignKey("dbo.Instrument", t => t.InstrumentID)
+                .ForeignKey("dbo.Lesson", t => t.LessonID)
+                .Index(t => t.InstrumentID)
+                .Index(t => t.LessonID);
+            
+            CreateTable(
+                "dbo.IdentityRole",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.IdentityUserRole",
+                c => new
+                    {
+                        UserId = c.String(nullable: false, maxLength: 128),
+                        RoleId = c.String(),
+                        IdentityRole_Id = c.String(maxLength: 128),
+                        ApplicationUser_Id = c.String(maxLength: 128),
+                    })
+                .PrimaryKey(t => t.UserId)
+                .ForeignKey("dbo.IdentityRole", t => t.IdentityRole_Id)
+                .ForeignKey("dbo.ApplicationUser", t => t.ApplicationUser_Id)
+                .Index(t => t.IdentityRole_Id)
+                .Index(t => t.ApplicationUser_Id);
+            
+            CreateTable(
+                "dbo.ApplicationUser",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        Email = c.String(),
+                        EmailConfirmed = c.Boolean(nullable: false),
+                        PasswordHash = c.String(),
+                        SecurityStamp = c.String(),
+                        PhoneNumber = c.String(),
+                        PhoneNumberConfirmed = c.Boolean(nullable: false),
+                        TwoFactorEnabled = c.Boolean(nullable: false),
+                        LockoutEndDateUtc = c.DateTime(),
+                        LockoutEnabled = c.Boolean(nullable: false),
+                        AccessFailedCount = c.Int(nullable: false),
+                        UserName = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.IdentityUserClaim",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        UserId = c.String(),
+                        ClaimType = c.String(),
+                        ClaimValue = c.String(),
+                        ApplicationUser_Id = c.String(maxLength: 128),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.ApplicationUser", t => t.ApplicationUser_Id)
+                .Index(t => t.ApplicationUser_Id);
+            
+            CreateTable(
+                "dbo.IdentityUserLogin",
+                c => new
+                    {
+                        UserId = c.String(nullable: false, maxLength: 128),
+                        LoginProvider = c.String(),
+                        ProviderKey = c.String(),
+                        ApplicationUser_Id = c.String(maxLength: 128),
+                    })
+                .PrimaryKey(t => t.UserId)
+                .ForeignKey("dbo.ApplicationUser", t => t.ApplicationUser_Id)
+                .Index(t => t.ApplicationUser_Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.IdentityUserRole", "ApplicationUser_Id", "dbo.ApplicationUser");
+            DropForeignKey("dbo.IdentityUserLogin", "ApplicationUser_Id", "dbo.ApplicationUser");
+            DropForeignKey("dbo.IdentityUserClaim", "ApplicationUser_Id", "dbo.ApplicationUser");
+            DropForeignKey("dbo.IdentityUserRole", "IdentityRole_Id", "dbo.IdentityRole");
+            DropForeignKey("dbo.Lesson", "InstrumentID", "dbo.Instrument");
+            DropForeignKey("dbo.Tablature", "LessonID", "dbo.Lesson");
+            DropForeignKey("dbo.Tablature", "InstrumentID", "dbo.Instrument");
+            DropIndex("dbo.IdentityUserLogin", new[] { "ApplicationUser_Id" });
+            DropIndex("dbo.IdentityUserClaim", new[] { "ApplicationUser_Id" });
+            DropIndex("dbo.IdentityUserRole", new[] { "ApplicationUser_Id" });
+            DropIndex("dbo.IdentityUserRole", new[] { "IdentityRole_Id" });
+            DropIndex("dbo.Tablature", new[] { "LessonID" });
+            DropIndex("dbo.Tablature", new[] { "InstrumentID" });
+            DropIndex("dbo.Lesson", new[] { "InstrumentID" });
+            DropTable("dbo.IdentityUserLogin");
+            DropTable("dbo.IdentityUserClaim");
+            DropTable("dbo.ApplicationUser");
+            DropTable("dbo.IdentityUserRole");
+            DropTable("dbo.IdentityRole");
+            DropTable("dbo.Tablature");
+            DropTable("dbo.Lesson");
+            DropTable("dbo.Instrument");
+        }
+    }
+}
---
+++ MusicHelp.Data/Migrations/Configuration.cs
@@ -0,0 +1,24 @@
+﻿namespace MusicHelp.Data.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<MusicHelp.Data.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "MusicHelp.Data.ApplicationDbContext";
+        }
+
+        protected override void Seed(MusicHelp.Data.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
